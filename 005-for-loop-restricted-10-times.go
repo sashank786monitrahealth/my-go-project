049df5c2cde781f1019efd9256962cc3a65eb92e
sashank786monitrahealth <sashank.bhogu@monitrahealth.com>
2018-05-06 17:10:13 +0530

    adding restricting to loop 10 times

---
+++ 005-for-loop-restricted-10-times.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  var myCounter int;
+  myCounter =0;
+   for myCounter < 10{
+     fmt.Printf("Hey There!\n");
+     myCounter += 1;
+   }
+
+}
