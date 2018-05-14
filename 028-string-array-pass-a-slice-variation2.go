e514ed78318e3b9691e2c6790d7832aae31abc56
sashank786monitrahealth <sashank.bhogu@monitrahealth.com>
2018-05-14 21:14:38 +0530

    more slice variations

---
+++ 028-string-array-pass-a-slice-variation2.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+  "fmt"
+)
+
+func myPrinter(w []string){
+  for _,word := range w {
+    fmt.Printf("%s ",word);
+  }
+  fmt.Printf("\n");
+
+  w[3] = "elephant"; // this does not change the value in the array
+
+}
+
+func main(){
+  words := [] string {"the","quick","brown","fox","jumped","over","the","lazy","log"};
+  myPrinter(words[0:4]);
+  myPrinter(words[3:9]); // prints till the end of array
+}
