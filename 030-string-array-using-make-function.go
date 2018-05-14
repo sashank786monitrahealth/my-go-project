4e3b8a581fa286006f5525c6718013415fdbba85
sashank786monitrahealth <sashank.bhogu@monitrahealth.com>
2018-05-14 21:36:56 +0530

    using make function

---
+++ 030-string-array-using-make-function.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "fmt"
+)
+
+func myPrinter(w []string){
+  for _,word := range w {
+    fmt.Printf("%s ",word);
+  }
+  fmt.Printf("\n");
+
+  w[3] = "elephant"; // this does not change the value in the array
+
+}
+
+func main(){
+  words := make([]string,9)
+  words[0] = "the";
+  words[1] = "quick";
+  words[2] = "brown";
+  words[3] = "fox";
+  words[4] = "jumps";
+  words[5] = "over";
+  words[6] = "the";
+  words[7] = "lazy";
+  words[8] = "dog";
+  myPrinter(words[0:4]);
+  myPrinter(words[3:9]); // prints till the end of array
+}
