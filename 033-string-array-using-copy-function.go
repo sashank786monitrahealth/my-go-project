4f6d75ab22e7a05caf212c7359058405efb97902
sashank786monitrahealth <sashank.bhogu@monitrahealth.com>
2018-05-14 23:53:14 +0530

    arrays - using make and copy functions

---
+++ 033-string-array-using-copy-function.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+  "fmt"
+)
+
+func myPrinter(w []string){
+  for _,word := range w {
+    fmt.Printf("%s ",word);
+  }
+  fmt.Printf("\n");
+
+  w[3] = "elephant"; // this does not change the value in the array
+
+}
+
+func main(){
+  words := make([]string,0,9)
+  fmt.Printf("len = %d , cap = %d\n\n", len(words), cap(words));
+  words = append(words,"the");
+  words = append(words,"quick");
+  words = append(words,"brown");
+  words = append(words,"fox");
+  words = append(words,"jumps");
+  words = append(words,"over");
+  words = append(words,"the");
+  words = append(words,"lazy");
+  words = append(words,"dog");
+  fmt.Printf("len = %d , cap = %d\n\n", len(words), cap(words));
+  myPrinter(words[0:4]);
+  myPrinter(words[3:9]); // prints till the end of array
+
+  myNewWords := make([]string,12);
+  copy(myNewWords,words);
+  fmt.Printf("\n myNewWords = %s\n\n",myNewWords);
+
+}
