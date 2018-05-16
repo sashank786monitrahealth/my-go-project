033b1887fdee035bb10e4023dc89ae37f69ed61a
sashank786monitrahealth <sashank.bhogu@monitrahealth.com>
2018-05-16 13:13:33 +0530

    another example of anonymousfunction assigned to variable

---
+++ 037-function-anonymous-example.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+  "fmt"
+)
+
+var (
+square = func(inp int) int {
+     return multiply2nos(inp, inp);
+}
+)
+
+var(
+multiply2nos = func(inp1, inp2 int) int {
+     return inp1 * inp2;
+}
+
+)
+
+func main(){
+  fmt.Printf("multiply2nos(15,6) = %d\n",multiply2nos(15,6));
+  fmt.Printf("square(36) = %d\n",square(36));
+}
