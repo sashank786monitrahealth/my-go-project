f35b482627774086ea166143cde79fb6b5ab591e
sashank786monitrahealth <sashank.bhogu@monitrahealth.com>
2018-05-23 15:33:08 +0530

    simulation of string concatenation and typecast to string

---
+++ 050-datatype-string-typecast.go
@@ -0,0 +1,14 @@
+package main;
+
+import(
+   "fmt";
+)
+
+func main(){
+   var myStr1 string;
+   var myStr2 string;
+   myStr1 = fmt.Sprintf("%v",16.04);
+   myStr2 = "My Ubuntu Version : "+myStr1+"\n"
+   fmt.Printf(myStr2);
+   fmt.Printf("length of above string = %d\n",len(myStr2));
+}
